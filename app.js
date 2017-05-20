3e0d8acef6d41d2a075d260d47bfe1d17418fd05
Jason <atxjdc@gmail.com>
2017-05-20 19:19:28 +0000

    updated to look like the mock up

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 var express = require("express");
 var app = express();
 
+app.use(express.static("public"));
+
 app.set("view engine", "ejs");
 
 app.get("/", function(req, res){
     res.render("home");
+});
+
+app.get("/contact", function(req, res){
+    res.render("contact");
+});
+
+app.get("/policies", function(req, res){
+    res.render("policies");
 });
 
 app.get("/about", function(req, res){
@@ -21,6 +31,10 @@ app.get("/console", function(req, res){
 
 app.get("/phase", function(req, res){
     res.render("phase");
+});
+
+app.get("*", function(req, res){
+    res.render("Undefined Path: This is the MiddleMatter site, but there is no page on this path.");
 });
 
 app.listen(process.env.PORT, process.env.IP);
